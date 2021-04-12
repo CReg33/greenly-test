0076b037e0ada10711f19ec0713229043b2b98cc
Cel <celineregnaud@gmail.com>
2021-04-12 21:08:55 +0200

    wrote specific methods in Store to refactor updateDiscount()

--- src/store.js
+++ src/store.js
@@ -2,6 +2,35 @@ export class Store {
   constructor(discountOffers = []) {
     this.discountOffers = discountOffers;
   }
+  
+  updateRegularDiscount(product, discount) {
+    if (product.expiresIn >= 0) {
+      product.decreaseDiscountInPercent(discount);
+    } else {
+      product.decreaseDiscountInPercent(discount * 2);
+    }
+  }
+  updateNaturaliaDiscount(product) {
+    if (product.expiresIn >= 0) {
+      product.increaseDiscountInPercent(1)
+    } else {
+      product.increaseDiscountInPercent(2)
+    }  
+  }
+  updateVintedDiscount(product) {
+    if (product.expiresIn > 10) {
+      product.ncreaseDiscountInPercent(1) 
+    } else if (product.expiresIn <= 10 && product.expiresIn > 5) {
+      product.increaseDiscountInPercent(2)
+    } else if (product.expiresIn <= 5 && product.expiresIn >= 0) {
+      product.increaseDiscountInPercent(3)
+    } else {
+      product.benefit = 0
+    }
+  }
+
+
+
   updateDiscounts() {
     for (var i = 0; i < this.discountOffers.length; i++) {
       if (
